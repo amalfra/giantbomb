ee67ff5b518b30e25ea3e486b24e2be12cfdb420
Amal Francis <amalfra@gmail.com>
2013-10-20 19:48:31 +0530

    added class structure

--- GaintBomb/GaintBomb.php
+++ GaintBomb/GaintBomb.php
@@ -1,10 +1,9 @@
 <?php
 /**
-*
 * GaintBomb PHP wrapper is a simple class written in PHP to
 * make interactions with GaintBomb api easier.
 *
-* @package    GaintBomb PHP wrapper
+* @package    GaintBomb api PHP wrapper
 * @version    0.1beta
 * @author     Amal Francis
 * @license    MIT License
