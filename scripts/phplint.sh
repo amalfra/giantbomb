2fd59d515fbcf0e89cbdeb679145b4a61bf0a91c
Amal Francis <amalfra@gmail.com>
2022-07-30 19:16:14 +0530

    add PHP_CodeSniffer

--- scripts/phplint.sh
+++ scripts/phplint.sh
@@ -1,8 +1,20 @@
 #! /bin/sh
 
-find . -iname '*.php' -path "./src/*" -or -path "./tests/*" | while read line; do
-  echo "Linting file - $line"
-  lint_cmd="$(php -l $line)"
+find . -iname '*.php' -path "./src/*" -or -path "./tests/*" | while read file; do
+  echo "Linting file - $file"
+  lint_cmd="$(php -l $file)"
+  rs=$?
+  if [ $rs != 0 ]; then
+    exit $rs
+  fi
+
+  echo "PHP_CodeSniffer file - $file"
+  lint_cmd="$(./vendor/bin/phpcs -h $file)"
+  rs=$?
+  if [ $rs != 0 ]; then
+    exit $rs
+  fi
+  lint_cmd="$(./vendor/bin/phpcbf -h $file)"
   rs=$?
   if [ $rs != 0 ]; then
     exit $rs
