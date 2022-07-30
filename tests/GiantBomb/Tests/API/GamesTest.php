961a12b87815f372972f7ecb11836db18bb190eb
Amal Francis <amalfra@gmail.com>
2022-07-30 15:54:41 +0530

    add remaining tests for games

--- tests/GiantBomb/Tests/API/GamesTest.php
+++ tests/GiantBomb/Tests/API/GamesTest.php
@@ -9,6 +9,17 @@ class GamesTest extends TestCase {
   // games() tests start
 
   /** @test */
+  public function gamesWithInvalidToken() {
+    $config = array(
+      'token' => 'invalid',
+    );
+    $giantbomb = new Client($config);
+    $resp = $giantbomb->games();
+
+    $this->assertEquals('Invalid API Key', $resp['error']);
+  }
+
+  /** @test */
   public function gamesWithoutOptions() {
     $config = array(
       'token' => getenv('GIANTBOMB_TESTS_API_KEY'),
@@ -91,5 +102,81 @@ class GamesTest extends TestCase {
     $this->assertEquals(13, count($resp['results']));
   }
 
+  /** @test */
+  public function gamesWithsortOption() {
+    $config = array(
+      'token' => getenv('GIANTBOMB_TESTS_API_KEY'),
+    );
+    $giantbomb = new Client($config);
+    $resp = $giantbomb->games(array(
+      'sort' => 'id:desc',
+    ));
+
+    $this->assertEquals('1.0', $resp['version']);
+    $this->assertEquals('OK', $resp['error']);
+    $this->assertEquals(100, $resp['limit']);
+    $this->assertEquals(0, $resp['offset']);
+    $this->assertEquals(100, count($resp['results']));
+  }
+
+  /** @test */
+  public function gamesWithfilterOption() {
+    $config = array(
+      'token' => getenv('GIANTBOMB_TESTS_API_KEY'),
+    );
+    $giantbomb = new Client($config);
+    $resp = $giantbomb->games(array(
+      'filter' => 'aliases:Desert Strike',
+    ));
+
+    $this->assertEquals('1.0', $resp['version']);
+    $this->assertEquals('OK', $resp['error']);
+    $this->assertEquals(100, $resp['limit']);
+    $this->assertEquals(0, $resp['offset']);
+    $this->assertEquals(1, count($resp['results']));
+    $this->assertEquals('Desert Strike: Return to the Gulf', $resp['results'][0]['name']);
+  }
+
   // games() tests end
+
+  // game() tests start
+
+  /** @test */
+  public function gameWithInvalidToken() {
+    $config = array(
+      'token' => 'invalid',
+    );
+    $giantbomb = new Client($config);
+    $resp = $giantbomb->game();
+
+    $this->assertEquals('Invalid API Key', $resp['error']);
+  }
+
+  /** @test */
+  public function gameWithoutId() {
+    $config = array(
+      'token' => getenv('GIANTBOMB_TESTS_API_KEY'),
+    );
+    $giantbomb = new Client($config);
+    $resp = $giantbomb->game();
+
+    $this->assertEquals('Object Not Found', $resp['error']);
+  }
+
+  /** @test */
+  public function game() {
+    $config = array(
+      'token' => getenv('GIANTBOMB_TESTS_API_KEY'),
+    );
+    $giantbomb = new Client($config);
+    $resp = $giantbomb->game(121);
+
+    $this->assertEquals('0', $resp['version']);
+    $this->assertEquals('OK', $resp['error']);
+    $this->assertEquals(1, $resp['limit']);
+    $this->assertEquals(0, $resp['offset']);
+    $this->assertEquals(121, $resp['results']['id']);
+  }
+
+  // game() tests end
 }
